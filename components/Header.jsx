b7489f8bd59239ad010327a176332c99205bc698
kkanim <cliffordanim100@gmail.com>
2026-02-04 23:24:18 +0000

    Added Header

---
+++ components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Heaeder() {
+    return (
+        <header>
+            <img src="../images/Chef Claude Icon.png" alt="Chef" />
+            <span>Chef Claude</span>
+        </header>
+    )
+}
