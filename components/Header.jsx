618380839d244c49808c46e6a7c94870ad59562b
kkanim <cliffordanim100@gmail.com>
2026-02-04 23:28:53 +0000

    Changed name

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,7 +2,7 @@ export default function Heaeder() {
     return (
         <header>
             <img src="../images/Chef Claude Icon.png" alt="Chef" />
-            <span>Chef Claude</span>
+            <span>Chef Cyclo</span>
         </header>
     )
 }
